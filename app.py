03e78bb25c90923c9ec3ddee07a486ee081b625a
Sebastian Fernandez Lopez <101682500+sebasfdezzz@users.noreply.github.com>
2023-11-27 14:33:05 -0600

    openai

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ def getCity(text):
 
 
 from flask import Flask, render_template, url_for
+import openai
 
 app = Flask(__name__)
+
+openai.api_key = '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'
 
 @app.route('/')
 def index():
@@ -57,5 +60,31 @@ def get_unsplash_image(city):
         return jsonify({'image_url': image_url})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+@app.route('/reason-to-go/', methods=['POST'])
+def get_gpt_reason():
+    try:
+        data = request.get_json()
+        input_text = data.get('input', '')
+        city = data.get('city', '')
+
+        if not input_text or not city:
+            return jsonify({'error': 'Both "input" and "city" parameters are required.'}), 400
+
+        prompt = f'Tell me why {city} is a good place to go given a person is looking for a place to vacation and this is their input: {input_text}'
+
+        response = openai.Completion.create(
+            engine="text-davinci-002", 
+            prompt=prompt,
+            max_tokens=150 
+        )
+
+        generated_reason = response['choices'][0]['text'].strip()
+
+        return jsonify({'generated_reason': generated_reason})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=3003)
