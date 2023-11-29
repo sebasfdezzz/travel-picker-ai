7c62370b7489bd1e14a3f0e0f66192cd75728c5d
Sebastian Fernandez Lopez <101682500+sebasfdezzz@users.noreply.github.com>
2023-11-29 15:52:35 -0600

    production ready

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ from keras.models import load_model
 
 
 
-train_model_bool = True
+train_model_bool = False
 file_name_model = 'DNN_model_best_2.h5'
-dev_mode=True
+dev_mode=False
 examples_length=100
 num_examples = 10
 
