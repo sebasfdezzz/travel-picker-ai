6e97b8215a0368e6a5b4ac300bfc82907bf09fb3
Sebastian Fernandez Lopez <101682500+sebasfdezzz@users.noreply.github.com>
2023-11-29 12:12:29 -0600

    new model

--- app2.py
+++ app2.py
@@ -41,7 +41,7 @@ from keras.models import load_model
 
 
 train_model_bool = True
-file_name_model = 'DNN_model_5.h5'
+file_name_model = 'LSTM_model_3.h5'
 dev_mode=True
 examples_length=50
 num_examples = 0 #auto
